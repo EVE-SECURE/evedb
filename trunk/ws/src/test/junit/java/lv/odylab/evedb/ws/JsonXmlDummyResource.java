1f8898265c86e839ae97224495d8c593a46e2170
vleushin <vleushin@20dc3c94-afd5-e942-f14b-94755896aafb>
2010-10-08 23:34:05 +0000

    EveDb:TRUNK -- Setting odylab-appengine to 1.11, small adjustments

--- trunk/ws/src/test/junit/java/lv/odylab/evedb/ws/JsonXmlDummyResource.java
+++ trunk/ws/src/test/junit/java/lv/odylab/evedb/ws/JsonXmlDummyResource.java
@@ -3,6 +3,7 @@ package lv.odylab.evedb.ws;
 import java.io.PrintWriter;
 
 public class JsonXmlDummyResource extends EveDbResource implements ProvidesJson, ProvidesXml {
+    private static final long serialVersionUID = 5170421454377245928L;
 
     @Override
     public void provideJson(String pathInfo, PrintWriter writer) {
