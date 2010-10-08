dbcc070db7dba410448fb8be4d62f151134cdc08
vleushin <vleushin@20dc3c94-afd5-e942-f14b-94755896aafb>
2010-10-08 23:34:05 +0000

    EveDb:TRUNK -- Setting odylab-appengine to 1.11, small adjustments

--- ws/src/test/junit/java/lv/odylab/evedb/ws/ExceptionDummyResource.java
+++ ws/src/test/junit/java/lv/odylab/evedb/ws/ExceptionDummyResource.java
@@ -3,6 +3,7 @@ package lv.odylab.evedb.ws;
 import java.io.PrintWriter;
 
 public class ExceptionDummyResource extends EveDbResource implements ProvidesText {
+    private static final long serialVersionUID = -6926641972674920367L;
 
     @Override
     public void provideText(String pathInfo, PrintWriter writer) {
--- ws/src/test/junit/java/lv/odylab/evedb/ws/XmlDummyResource.java
+++ ws/src/test/junit/java/lv/odylab/evedb/ws/XmlDummyResource.java
@@ -3,6 +3,7 @@ package lv.odylab.evedb.ws;
 import java.io.PrintWriter;
 
 public class XmlDummyResource extends EveDbResource implements ProvidesXml {
+    private static final long serialVersionUID = -6186469133177173786L;
 
     @Override
     public void provideXml(String pathInfo, PrintWriter writer) {
