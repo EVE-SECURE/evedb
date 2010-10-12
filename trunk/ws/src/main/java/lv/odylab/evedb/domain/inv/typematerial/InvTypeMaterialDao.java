5f368d1a901981b34ac5e3ab458f567f4ab6183d
vleushin <vleushin@20dc3c94-afd5-e942-f14b-94755896aafb>
2010-10-12 17:08:39 +0000

    EveDb:TRUNK Updates Issue 19 -- Moving core to ws

---
+++ trunk/ws/src/main/java/lv/odylab/evedb/domain/inv/typematerial/InvTypeMaterialDao.java
@@ -0,0 +1,49 @@
+package lv.odylab.evedb.domain.inv.typematerial;
+
+import com.googlecode.objectify.ObjectifyService;
+import lv.odylab.evedb.domain.IdNotFoundException;
+import lv.odylab.evedb.domain.NameNotFoundException;
+
+import java.util.List;
+
+public class InvTypeMaterialDao {
+
+    static {
+        ObjectifyService.register(InvTypeMaterial.class);
+    }
+
+    private final String dumpVersion;
+
+    public InvTypeMaterialDao(String dumpVersion) {
+        this.dumpVersion = dumpVersion;
+    }
+
+    public List<InvTypeMaterial> getForTypeID(Long typeID) {
+        List<InvTypeMaterial> invTypeMaterials = ObjectifyService.begin().query(InvTypeMaterial.class)
+                .filter("dumpVersion", dumpVersion)
+                .filter("typeID", typeID)
+                .order("materialTypeID").list();
+        if (invTypeMaterials.isEmpty()) {
+            throw new IdNotFoundException(typeID);
+        }
+        return invTypeMaterials;
+    }
+
+    public List<InvTypeMaterial> getForTypeIdWithoutCheck(Long typeID) {
+        return ObjectifyService.begin().query(InvTypeMaterial.class)
+                .filter("dumpVersion", dumpVersion)
+                .filter("typeID", typeID)
+                .order("materialTypeID").list();
+    }
+
+    public List<InvTypeMaterial> getForTypeName(String typeName) {
+        List<InvTypeMaterial> invTypeMaterials = ObjectifyService.begin().query(InvTypeMaterial.class)
+                .filter("dumpVersion", dumpVersion)
+                .filter("typeName", typeName)
+                .order("materialTypeID").list();
+        if (invTypeMaterials.isEmpty()) {
+            throw new NameNotFoundException(typeName);
+        }
+        return invTypeMaterials;
+    }
+}
